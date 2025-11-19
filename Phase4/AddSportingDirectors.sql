7b50b422f6c181683d13982198b775b5a8c56460
AveerAlamchand <aalamchand052@gmail.com>
2025-11-19 17:04:53 +0200

    Add files via upload

---
+++ Phase4/AddSportingDirectors.sql
@@ -0,0 +1,289 @@
+-- OP001 signed by the Sporting Director of AJA
+CALL InsertSportingDirector(
+  'SD_AJA', 8.0,                  -- SportNetSpend = releaseClause
+  'John', 'Cena',                 -- Name/Surname from Manager
+  80.0, 2025, 'January',          -- TransferBudget, Year, Month
+  'AJA', 'OP001'                  -- ClubID, PIDNumber
+);
+
+-- OP002 signed by the Sporting Director of ARS
+CALL InsertSportingDirector(
+  'SD_ARS', 2.0,
+  'Sergei', 'Morozov',
+  20.0, 2025, 'January',
+  'ARS', 'OP002'
+);
+
+-- OP003 signed by the Sporting Director of AST
+CALL InsertSportingDirector(
+  'SD_AST', 3.5,
+  'Kenji', 'Nakamura',
+  35.0, 2025, 'January',
+  'AST', 'OP003'
+);
+
+-- OP004 also by Sporting Director AJA (multiple signings)
+CALL InsertSportingDirector(
+  'SD_AJA', 1.5,
+  'Rafael', 'Mendes',
+  15.0, 2025, 'January',
+  'AJA', 'OP004'
+);
+
+-- OP005 signed by the Sporting Director of ATH
+CALL InsertSportingDirector(
+  'SD_ATH', 5.0,
+  'Thanh', 'Le',
+  50.0, 2025, 'January',
+  'ATH', 'OP005'
+);
+
+-- OP006 by Sporting Director of ATM
+CALL InsertSportingDirector(
+  'SD_ATM', 4.0,
+  'Esteban', 'Ortiz',
+  40.0, 2025, 'January',
+  'ATM', 'OP006'
+);
+
+-- OP007 by Sporting Director of AZA
+CALL InsertSportingDirector(
+  'SD_AZA', 2.8,
+  'Abdoulaye', 'Faye',
+  28.0, 2025, 'January',
+  'AZA', 'OP007'
+);
+
+-- OP008 by Sporting Director of BAR
+CALL InsertSportingDirector(
+  'SD_BAR', 3.0,
+  'Omar', 'Al-Zein',
+  30.0, 2025, 'January',
+  'BAR', 'OP008'
+);
+
+-- OP009 by Sporting Director of BAY
+CALL InsertSportingDirector(
+  'SD_BAY', 1.8,
+  'Esteban', 'Ortiz',
+  18.0, 2025, 'January',
+  'BAY', 'OP009'
+);
+
+-- OP010 by Sporting Director of BEN
+CALL InsertSportingDirector(
+  'SD_BEN', 2.2,
+  'James', 'Dsouza',
+  22.0, 2025, 'January',
+  'BEN', 'OP010'
+);
+
+-- OP011 by Sporting Director of BET
+CALL InsertSportingDirector(
+  'SD_BET', 3.7,
+  'Felix', 'Gruber',
+  37.0, 2025, 'January',
+  'BET', 'OP011'
+);
+
+-- OP012 also by Sporting Director BAR (multiple signings)
+CALL InsertSportingDirector(
+  'SD_BAR', 6.0,
+  'Omar', 'Al-Zein',
+  60.0, 2025, 'January',
+  'BAR', 'OP012'
+);
+
+-- OP013 also by Sporting Director BET
+CALL InsertSportingDirector(
+  'SD_BET', 2.1,
+  'Felix', 'Gruber',
+  21.0, 2025, 'January',
+  'BET', 'OP013'
+);
+
+-- OP014 by Sporting Director of BOR
+CALL InsertSportingDirector(
+  'SD_BOR', 2.9,
+  'Youssef', 'Khaled',
+  29.0, 2025, 'January',
+  'BOR', 'OP014'
+);
+
+-- OP015 by Sporting Director of BRA
+CALL InsertSportingDirector(
+  'SD_BRA', 1.6,
+  'Liang', 'Zheng',
+  16.0, 2025, 'January',
+  'BRA', 'OP015'
+);
+
+-- OP016 by Sporting Director of CHE
+CALL InsertSportingDirector(
+  'SD_CHE', 4.7,
+  'Adel', 'Barghouti',
+  47.0, 2025, 'January',
+  'CHE', 'OP016'
+);
+
+-- OP017 by Sporting Director of DOR
+CALL InsertSportingDirector(
+  'SD_DOR', 2.4,
+  'Bruno', 'Silva',
+  24.0, 2025, 'January',
+  'DOR', 'OP017'
+);
+
+-- OP018 by Sporting Director of ESP
+CALL InsertSportingDirector(
+  'SD_ESP', 3.1,
+  'Khalid', 'Al-Hamadi',
+  31.0, 2025, 'January',
+  'ESP', 'OP018'
+);
+
+-- OP019 by Sporting Director of EVE
+CALL InsertSportingDirector(
+  'SD_EVE', 3.8,
+  'Ravi', 'Menon',
+  38.0, 2025, 'January',
+  'EVE', 'OP019'
+);
+
+-- OP020 by Sporting Director of FEY
+CALL InsertSportingDirector(
+  'SD_FEY', 2.0,
+  'Colin', 'Bradley',
+  20.0, 2025, 'January',
+  'FEY', 'OP020'
+);
+
+-- OP021 by Sporting Director of FIO
+CALL InsertSportingDirector(
+  'SD_FIO', 2.7,
+  'Lars', 'Olsson',
+  27.0, 2025, 'January',
+  'FIO', 'OP021'
+);
+
+-- OP022 by Sporting Director of FRE
+CALL InsertSportingDirector(
+  'SD_FRE', 3.4,
+  'Minho', 'Choi',
+  34.0, 2025, 'January',
+  'FRE', 'OP022'
+);
+
+-- OP023 by Sporting Director of GEN
+CALL InsertSportingDirector(
+  'SD_GEN', 5.1,
+  'Tunde', 'Okeke',
+  51.0, 2025, 'January',
+  'GEN', 'OP023'
+);
+
+-- OP024 by Sporting Director of GRO
+CALL InsertSportingDirector(
+  'SD_GRO', 3.6,
+  'Zayd', 'Khalil',
+  36.0, 2025, 'January',
+  'GRO', 'OP024'
+);
+
+-- OP025 by Sporting Director of GUI
+CALL InsertSportingDirector(
+  'SD_GUI', 2.5,
+  'Bruno', 'Silva',
+  25.0, 2025, 'January',
+  'GUI', 'OP025'
+);
+
+-- OP026 by Sporting Director of HEE
+CALL InsertSportingDirector(
+  'SD_HEE', 3.2,
+  'Nikos', 'Karras',
+  32.0, 2025, 'January',
+  'HEE', 'OP026'
+);
+
+-- OP027 by Sporting Director of HER
+CALL InsertSportingDirector(
+  'SD_HER', 1.5,
+  'Kristoffer', 'Lind',
+  15.0, 2025, 'January',
+  'HER', 'OP027'
+);
+
+-- OP028 by Sporting Director of INT
+CALL InsertSportingDirector(
+  'SD_INT', 2.0,
+  'Youssef', 'Khaled',
+  20.0, 2025, 'January',
+  'INT', 'OP028'
+);
+
+-- OP029 by Sporting Director of JUV
+CALL InsertSportingDirector(
+  'SD_JUV', 4.2,
+  'James', 'Dsouza',
+  42.0, 2025, 'January',
+  'JUV', 'OP029'
+);
+
+-- OP030 by Sporting Director of LAZ
+CALL InsertSportingDirector(
+  'SD_LAZ', 3.9,
+  'Ivan', 'Hrvoje',
+  39.0, 2025, 'January',
+  'LAZ', 'OP030'
+);
+
+SELECT * FROM SPORTINGDIRECTOR;
+DELETE FROM SPORTINGDIRECTOR;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE Report1(IN inYear   INT,IN inMonth  VARCHAR(9))
+BEGIN
+-- 1) Totals per club, pick top 5
+CREATE TEMPORARY TABLE IF NOT EXISTS club_totals AS
+SELECT ClubID, SUM(SportNetSpend) AS TotalSpent
+FROM SPORTINGDIRECTOR
+WHERE Year  = inYear
+AND Month = inMonth
+GROUP BY ClubID
+ORDER BY TotalSpent DESC
+LIMIT 5;
+
+-- 2) Highest single purchase per club
+CREATE TEMPORARY TABLE IF NOT EXISTS club_max AS
+SELECT SD.ClubID,
+SD.PIDNumber,
+SD.SportNetSpend AS LargestPurchase
+FROM SPORTINGDIRECTOR SD
+JOIN (
+SELECT ClubID,
+MAX(SportNetSpend) AS MaxSpend
+FROM SPORTINGDIRECTOR
+WHERE Year  = inYear
+AND Month = inMonth
+GROUP BY ClubID
+) AS mx
+ON SD.ClubID      = mx.ClubID
+AND SD.SportNetSpend = mx.MaxSpend;
+
+-- 3) Final report: join totals, max, club names and player names
+SELECT C.ClubName, ct.TotalSpent, cm.LargestPurchase, P.Name AS TopPlayerFirst,P.Surname AS TopPlayerLast
+FROM club_totals AS ct
+JOIN FOOTBALLCLUB AS C ON ct.ClubID = C.ClubID
+JOIN club_max    AS cm ON ct.ClubID = cm.ClubID
+JOIN PLAYER      AS P  ON cm.PIDNumber = P.PIDNumber
+ORDER BY ct.TotalSpent DESC;
+
+DROP TEMPORARY TABLE IF EXISTS club_totals;
+DROP TEMPORARY TABLE IF EXISTS club_max;
+
+END$$
+DELIMITER ;
+
